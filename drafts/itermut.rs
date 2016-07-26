a0fee536bcbc740f049a53460438b348b8c8d6f4
cbreeden <github@u.breeden.cc>
2016-07-25 19:50:57 -0500

    Use easier example for mutable iterator first

--- drafts/itermut.rs
+++ drafts/itermut.rs
@@ -2,8 +2,66 @@
 
 // # Mutable Iterators
 
+// ## Part 1
 
+// To start out our introduction to constructing mutable iterators, we will use
+// a simple data structure consisting nothing but a vector of integers.  But
+// these are our integers, so they are special, which is why we will now call
+// them out `SpecialIntegers` type.
+
+struct SpecialIntegers(Vec<u32>);
+
+// To implement an iterator, we essentially need to tell Rust two things:
+//   a. What kind of elements will we be iterating over.  When iterating
+//      over our structure, we wil want our special integers (which are `u32`).
+//   b. What is the next element when we are iterating.
 //
+// We will therefore have the responsibility of keeping track of where we are at
+// while iterating.  We will do this by constructing what is
+// oftern called an "iterator interface".  To keep track of our iteration, we will
+// need our `SpecialIntegers` data structure and a cursor to keep track of where
+// we are.
+
+struct SpecialIntegersIterator<'a> {
+    data: &'a mut SpecialIntegers,
+    cursor: usize,
+}
+
+// We now need to tell rust how to get the next element in our iterator, which
+// we accomplish by implementing the `Iterator` trait on our interator inteface.
+// There is a problem that we will run into however.  Our iterator interface
+// has a mutable borrow of our `SpecialIntegers` data structure.  So while we
+// are holding this mutable borrow, we aren't technically allowed to give a mutable
+// reference to elements in our list.  We therefore will be required to use unsafe.
+// But before we do so, we should stop and think for a second: is what I am doing
+// actually safe?
+
+impl<'a> Iterator for &'a mut SpecialIntegersIterator<'a> {
+    // The type of object we will be iterating over
+    type Item = &'a mut u32;
+    fn next(&mut self) -> Option<&'a mut u32> {
+        unimplemented!();
+    }
+}
+
+// However, before we will be able to use this iterator interface, unless you 
+// are planning on creating a brand new `SpecialIntegersIterator` structure
+// each you want to use an iterator, we need to tell Rust how to make this structure
+// for wehenever we need an iterator.  We do this by implementing the `IntoIterator`
+// trait.  Which, less, tells rust how to make an iterator interface by calling
+// `iter_mut`.
+
+impl<'a> IntoIterator for &'a mut SpecialIntegers {
+    type Item =  &'a mut u32;
+    type IntoIter = SpecialIntegers;
+    
+    fn into_iter(&mut self) -> &'a mut SpecialIntegersIterator<'a> {
+        unimplemented!();
+    }
+}
+
+// ## Part 2
+
 // We will implement a simple tree that could, say, represent a simple DOM
 // structure.  You may notice that we don't actually allow for text in our
 // simple representation, but this isn't important here, what we really want
